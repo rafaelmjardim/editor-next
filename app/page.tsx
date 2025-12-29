3353782ec88d5b011335167447cdcc67e5f27458
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-12-29 14:19:57 -0300

    fix: remove importacao

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
   const [text, setText] = useState("");
